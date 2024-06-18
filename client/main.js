5c1acb35d63f57ccc77f2fb76e684ec0b6598ee6
Ramunas Kazlauskas <akuyra1@gmail.com>
2024-06-18 09:54:43 +0100

    nav bar done

--- client/main.js
+++ client/main.js
@@ -1,3 +1,21 @@
-//just a walking skeleton
-
 console.log("Testing!");
+
+
+
+//Hamburger icon animation function 
+const container = document.getElementById('hamb-container');
+const ul = document.getElementById('show');
+container.addEventListener("click", function() {
+    this.classList.toggle("change")
+    // ul.classList.toggle("nav-mob-container")
+    ul.classList.toggle("showing"); // Toggle 'open' class on ul (mobile nav)
+
+    // Optional: Adjust display property if you need inline/block display
+    if (ul.classList.contains("showing")) {
+        ul.style.display = "block";
+    } else {
+        ul.style.display = "none";
+    }
+})
+
+
