72f197d524cf4bb397ecdac9c9e6251945a24cbe
Ramunas Kazlauskas <akuyra1@gmail.com>
2024-06-18 14:14:52 +0100

    search bar and button done

--- client/main.js
+++ client/main.js
@@ -6,16 +6,12 @@ console.log("Testing!");
 const container = document.getElementById('hamb-container');
 const ul = document.getElementById('show');
 container.addEventListener("click", function() {
-    this.classList.toggle("change")
-    // ul.classList.toggle("nav-mob-container")
-    ul.classList.toggle("showing"); // Toggle 'open' class on ul (mobile nav)
+    this.classList.toggle("showing")
 
-    // Optional: Adjust display property if you need inline/block display
     if (ul.classList.contains("showing")) {
         ul.style.display = "block";
     } else {
         ul.style.display = "none";
     }
 })
-
 
