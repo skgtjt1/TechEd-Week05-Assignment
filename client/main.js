c0fc42cf52bbab562d7861696a436b399e83ba3d
Ramunas Kazlauskas <akuyra1@gmail.com>
2024-06-19 21:13:25 +0100

    fixed the responsive design of splash page

--- client/main.js
+++ client/main.js
@@ -5,13 +5,9 @@ console.log("Testing!");
 //Hamburger icon animation function 
 const container = document.getElementById('hamb-container');
 const ul = document.getElementById('show');
-container.addEventListener("click", function() {
-    this.classList.toggle("showing")
 
-    if (ul.classList.contains("showing")) {
-        ul.style.display = "block";
-    } else {
-        ul.style.display = "none";
-    }
-})
+container.addEventListener("click", function() {
+    this.classList.toggle("change");
+    ul.classList.toggle("nav-mob-container");
+});
 
